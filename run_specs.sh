9c66f4dd02dc169766bc016938231767a93eb79b
Craig Morton <CraigMorton92@gmail.com>
2016-02-28 18:42:35 +0000

    run_specs.sh heads and tails test run output with # filler line and test file name

--- run_specs.sh
+++ run_specs.sh
@@ -1,1 +1,1 @@
-for file in specs/*.rb; do ruby $file; echo $file; echo "#########################"; done 
+for file in specs/*.rb; do echo "#########################"; echo $file; ruby $file; echo $file; echo "#########################"; done
